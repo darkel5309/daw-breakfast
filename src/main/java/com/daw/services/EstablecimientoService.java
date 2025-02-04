0aa1241d30b95c03c6f0bf1ae78cdb192714997e
adri999jr <adri999jr123@gmail.com>
2025-02-04 09:16:37 +0100

    empiezo con establecimiento service

--- src/main/java/com/daw/services/EstablecimientoService.java
+++ src/main/java/com/daw/services/EstablecimientoService.java
@@ -1,5 +1,36 @@
 package com.daw.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.daw.persistence.entities.Establecimiento;
+import com.daw.persistence.repositories.EstablecimientoRepository;
+import com.daw.services.dto.EstablecimientoDTO;
+import com.daw.services.mappers.EstablecimientoMapper;
+
+
+
+
+@Service
 public class EstablecimientoService {
 
+	@Autowired
+	private EstablecimientoRepository establecimientoRepository;
+	
+	public List<EstablecimientoDTO> findAll(){
+		List<EstablecimientoDTO> establecimientoDTO = new ArrayList<EstablecimientoDTO>();
+		
+		for(Establecimiento e : this.establecimientoRepository.findAll()) {
+			establecimientoDTO.add(EstablecimientoMapper.toDTO(e));
+		}
+
+		return establecimientoDTO;
+	}
+	
+	
+	
+	
+	
 }
